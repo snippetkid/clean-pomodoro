7e963576423387c0a191c2d70ce3bec5176c8006
Dipin Jagadish <djagadish@outlook.com>
2024-05-16 16:11:14 +0530

    feat: add pomodoro limit

--- src/components/Pomodoro.tsx
+++ src/components/Pomodoro.tsx
@@ -1,31 +1,42 @@
 import { useEffect, useRef, useState } from "react";
 import { Timer } from "./Timer";
+import useInterval from "use-interval";
 
 export const Pomodoro = () => {
+  const LIMIT_MIN = 55;
   const [elapsedSeconds, setElapsedSeconds] = useState(0);
-
   const [start, setStart] = useState(false);
-
   const prev = useRef(-1);
 
   const minutes = Math.floor(elapsedSeconds / 60);
   const seconds = elapsedSeconds % 60;
 
-  useEffect(() => {
-    if (start) {
-      const intervalId = setInterval(() => {
+  const isComplete = () => LIMIT_MIN === elapsedSeconds;
+
+  useInterval(() => {
+    if (!isComplete()) {
+      if (start) {
         prev.current = prev.current === seconds ? -1 : seconds;
         setElapsedSeconds((value) => value + 1);
-      }, 1000);
-      return () => clearInterval(intervalId);
-    } else {
-      prev.current = -1;
+      } else {
+        prev.current = -1;
+      }
     }
-  }, [seconds, start]);
+  }, 2000);
+
+  const setTimerStatus = () => {
+    if (isComplete()) {
+      setElapsedSeconds(0);
+      setStart(!start);
+    } else {
+      setStart(!start);
+    }
+  };
 
   return (
-    <div style={{ cursor: "pointer" }} onClick={() => setStart(!start)}>
+    <div style={{ cursor: "pointer" }} onClick={() => setTimerStatus()}>
       <Timer
+        complete={isComplete()}
         running={start}
         previous={prev.current}
         minutes={minutes}
